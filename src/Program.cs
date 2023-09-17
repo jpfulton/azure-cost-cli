c3b3b718a7659a3a8058869bfb1418408ca6dd0c
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-09-16 20:53:27 -0500

    Update HttpClient max timeout.

--- src/Program.cs
+++ src/Program.cs
@@ -17,12 +17,14 @@ registrations.AddHttpClient("CostApi", client =>
 {
   client.BaseAddress = new Uri("https://management.azure.com/");
   client.DefaultRequestHeaders.Add("Accept", "application/json");
+  client.Timeout = TimeSpan.MaxValue;
 }).AddPolicyHandler(PollyPolicyExtensions.GetRetryAfterPolicy());
 
 registrations.AddHttpClient("RegionsApi", client =>
 {
   client.BaseAddress = new Uri("https://datacenters.microsoft.com/");
   client.DefaultRequestHeaders.Add("Accept", "application/json");
+  client.Timeout = TimeSpan.MaxValue;
 }).AddPolicyHandler(PollyPolicyExtensions.GetRetryAfterPolicy());
 
 
